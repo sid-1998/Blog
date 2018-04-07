2c68e0e254b2b13cbe296037ace3ff6b4ff78e4f
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2018-04-08 03:40:39 +0530

    new commit
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- blog/forms.py
+++ blog/forms.py
@@ -7,18 +7,10 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('author','title','text')
 
-        widgets = {
-        'title':forms.TextInput(attrs={'class':'textinputclass'}),
-        'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'}),
-        }
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
 
         model = Comment
         fields = ('author','text')
-
-        widgets = {
-        'author':forms.TextInput(attrs={'class':'textinputclass'}),
-        'text': forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
-        }
